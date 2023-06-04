f21b82242d42f2f8d97ed308c3bc7cd3e56f6872
iiKulakov <sevpor26@gmail.com>
2023-06-04 12:51:15 -0500

    add: log to errors

--- src/fetcher.ts
+++
@@ -1,9 +0,0 @@
-async function fetchArrayBuffer(url: string): Promise<ArrayBuffer> {
-  return fetch(url).then((response) => response.arrayBuffer())
-}
-
-const Fetcher = {
-  fetchArrayBuffer,
-}
-
-export default Fetcher
